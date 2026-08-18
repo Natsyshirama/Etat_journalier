cc7ddde1a715d82f7385f06775e1a5dcd9ab9411
Natsyshirama <fandresenajoy@gmail.com>
2026-08-18 11:52:13 +0300

    modif

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,9 +2,9 @@ module.exports = {
   apps: [
     {
       name: "fastapi-backend",
-      script: "D:\\marc\\GAB\\Etat_journalier\\.venv\\Scripts\\python.exe",
+      script: "/var/www/money/Etat_journalier/.venv/bin/python",
       args: "-m uvicorn app:app --host 0.0.0.0 --port 8000 --workers 4",
-      cwd: "D:\\marc\\GAB\\Etat_journalier\\back_end",
+      cwd: "/var/www/money/Etat_journalier/back_end",
       autorestart: true,
       watch: false
     }
