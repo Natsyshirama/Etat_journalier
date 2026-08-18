f7ad1810f6f1d598e4f1a2148f48d8b3124c7ce4
Natsyshirama <fandresenajoy@gmail.com>
2026-08-18 15:30:57 +0300

    changement

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000', // ton IP de backend Flask
+  baseURL: 'http://10.192.251.170:8000', // ton IP de backend Flask
   headers: {
     'Content-Type': 'application/json',
   },
